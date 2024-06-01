2ba759c3cbac63c1e24d7d7e3587f65e54e74d19
Saumil <31678223+sam02425@users.noreply.github.com>
2024-06-01 03:54:43 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Camera IDs
+LEFT_CAMERA_ID = 0
+RIGHT_CAMERA_ID = 1
+
+# YOLO model paths
+YOLO_CONFIG = 'yolo.cfg'
+YOLO_WEIGHTS = 'yolo.weights'
+COCO_NAMES = 'coco.names'
+
+# Calibration file paths
+LEFT_CALIBRATION_FILE = 'left_calibration.pkl'
+RIGHT_CALIBRATION_FILE = 'right_calibration.pkl'
+STEREO_CALIBRATION_FILE = 'stereo_calibration.pkl'
